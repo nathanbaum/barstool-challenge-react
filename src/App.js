4d5d183ec6464b998062819b11d62c87c69a5c61
Nathan Baum <nbaum510@gmail.com>
2018-08-23 16:10:24 -0400

    parameterized all component content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { BoxScore } from './components/BoxScore';
@@ -8,7 +7,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BoxScore/>
+        <BoxScore
+          labels={{
+            units: [1,2,3,4,5,6,7,8,9],
+            results: ['R', 'H', 'E'],
+          }}
+          away={{
+            name: "Cubs",
+            shortName: "CHC",
+            units: [1,0,2,0,0,0,0,1,1],
+            results: [5,12,0],
+          }}
+          home={{
+            name: "Cardinals",
+            shortName: "STL",
+            units: [0,0,0,3,0,0,0,0,1],
+            results: [4,8,1],
+          }}
+          gameState={"BTM\n9th"}
+          />
       </div>
     );
   }
--- src/components/BoxScore.js
+++ src/components/BoxScore.js
@@ -5,86 +5,79 @@ import './BoxScore.css';
 class BoxScore extends Component {
 
   render() {
+    let uniqueKey = 0;
     return (
       <div className="boxscore">
         <div className="boxscore__team boxscore__team--header">
           <label></label>
           <div className="boxscore__team__units">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
+            { this.props.labels.units.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
           <div className="boxscore__team__results">
-            <span>R</span>
-            <span>H</span>
-            <span>E</span>
+            { this.props.labels.results.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
         </div>
         <div className="boxscore__team boxscore__team--away">
-          <label>CHC</label>
+          <label>{this.props.away.shortName}</label>
           <div className="boxscore__team__units">
-            <span>1</span>
-            <span>0</span>
-            <span>2</span>
-            <span>0</span>
-            <span>0</span>
-            <span>0</span>
-            <span>0</span>
-            <span>1</span>
-            <span>1</span>
+            { this.props.away.units.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
           <div className="boxscore__team__results">
-            <span>5</span>
-            <span>12</span>
-            <span>0</span>
+            { this.props.away.results.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
         </div>
         <div className="boxscore__team boxscore__team--home">
-          <label>STL</label>
+          <label>{this.props.home.shortName}</label>
           <div className="boxscore__team__units">
-            <span>0</span>
-            <span>0</span>
-            <span>0</span>
-            <span>3</span>
-            <span>0</span>
-            <span>0</span>
-            <span>0</span>
-            <span>0</span>
-            <span>1</span>
+            { this.props.home.units.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
           <div className="boxscore__team__results">
-            <span>4</span>
-            <span>8</span>
-            <span>1</span>
+            { this.props.home.results.map( (e) => { return (<span key={uniqueKey++}>{e}</span>); } ) }
           </div>
         </div>
         <div className="boxscore__details">
           <div className="boxscore__details__team boxscore__details__team--away">
             <p>
-              <strong>Cubs</strong><small>CHC</small>
+              <strong>{this.props.away.name}</strong><small>{this.props.away.shortName}</small>
             </p>
-            <span>56-38</span>
           </div>
           <div className="boxscore__details__info">
-            <strong>Btm<br/>{'9th'}</strong>
+            <strong>{this.props.gameState}</strong>
           </div>
           <div className="boxscore__details__team boxscore__details__team--home">
             <p>
-              <strong>Cardinals</strong><small>STL</small>
+              <strong>{this.props.home.name}</strong><small>{this.props.home.shortName}</small>
             </p>
-            <span>56-38</span>
           </div>
         </div>
       </div>
     );
   }
-
 }
+
+BoxScore.propTypes = {
+
+  labels: PropTypes.shape({
+    units: PropTypes.array,
+    results: PropTypes.array,
+  }),
+
+  home: PropTypes.shape({
+    name: PropTypes.string,
+    shortName: PropTypes.string,
+    units: PropTypes.array,
+    results: PropTypes.array,
+  }),
+
+  away: PropTypes.shape({
+    name: PropTypes.string,
+    shortName: PropTypes.string,
+    units: PropTypes.array,
+    results: PropTypes.array,
+  }),
+
+  gameState: PropTypes.string,
+
+};
 
 export { BoxScore };
