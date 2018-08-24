7b06711f979f42d2da052f5de3cd7489d5393176
Nathan Baum <nbaum510@gmail.com>
2018-08-23 22:56:52 -0400

    finished react app

--- src/App.js
+++ src/App.js
@@ -4,28 +4,119 @@ import './App.css';
 import { BoxScore } from './components/BoxScore';
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      games: null
+    };
+
+    fetch('http://localhost:8000/api/games/all')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        // console.log('got something from the server:');
+        // console.log(json);
+        this.setState({games: json});
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   render() {
+
+    const insideContent = [];
+
+    if( this.state.games ) {
+      for( const g in this.state.games ) {
+
+        const game = this.state.games[g];
+        const unitLabels = [];
+        const resultLabels = [];
+        const homeResults = [];
+        const homeUnits = [];
+        const awayResults = [];
+        const awayUnits = [];
+        let gameState = '';
+        //in case there are any other types of sports like football etc
+        switch (game.league.alias) {
+
+          case 'MLB':
+            resultLabels.push('R', 'H', 'E');
+            homeResults.push(0,0,0);
+            game.homeTeamDetails.forEach((e) => {
+              unitLabels.push(e.number);
+              homeUnits.push(e.runs);
+              homeResults[0] += e.runs;
+              homeResults[1] += e.hits;
+              homeResults[2] += e.entryErrors;
+            });
+            awayResults.push(0,0,0);
+            game.awayTeamDetails.forEach((e) => {
+              awayUnits.push(e.runs);
+              awayResults[0] += e.runs;
+              awayResults[1] += e.hits;
+              awayResults[2] += e.entryErrors;
+            });
+            gameState += game.currentPeriodHalf === 'T' ? 'TOP ' : 'BTM ';
+            gameState += game.currentPeriod;
+            break;
+
+          default:
+            break;
+        }
+        switch (JSON.stringify(parseInt(gameState.charAt(gameState.length-1), 10))) {
+          case '1':
+            gameState += (parseInt(gameState.charAt(gameState.length-2), 10) === 1 ? 'th' : 'st');
+            break;
+          case '2':
+            gameState += (parseInt(gameState.charAt(gameState.length-2), 10) === 1 ? 'th' : 'nd');
+            break;
+          case '3':
+            gameState += (parseInt(gameState.charAt(gameState.length-2), 10) === 1 ? 'th' : 'rd');
+            break;
+          case 'null':
+            break;
+          default:
+            gameState += 'th';
+            break;
+        }
+        insideContent.push(
+          <BoxScore
+            key={game.foreignId}
+            labels={{
+              units: unitLabels,
+              results: resultLabels,
+            }}
+            away={{
+              name: game.awayTeam.name,
+              shortName: game.awayTeam.abbr,
+              units: awayUnits,
+              results: awayResults,
+            }}
+            home={{
+              name: game.homeTeam.name,
+              shortName: game.homeTeam.abbr,
+              units: homeUnits,
+              results: homeResults,
+            }}
+            gameState={gameState}
+            />
+        );
+
+      }
+    }
+    else {
+      insideContent.push(
+        <div key={0}>{'loading...'}</div>
+      );
+    }
+
     return (
       <div className="App">
-        <BoxScore
-          labels={{
-            units: [1,2,3,4,5,6,7,8,9],
-            results: ['R', 'H', 'E'],
-          }}
-          away={{
-            name: "Cubs",
-            shortName: "CHC",
-            units: [1,0,2,0,0,0,0,1,1],
-            results: [5,12,0],
-          }}
-          home={{
-            name: "Cardinals",
-            shortName: "STL",
-            units: [0,0,0,3,0,0,0,0,1],
-            results: [4,8,1],
-          }}
-          gameState={"BTM\n9th"}
-          />
+        {insideContent}
       </div>
     );
   }
